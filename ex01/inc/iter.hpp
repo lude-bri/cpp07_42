a3ce6c7504fe9c47728f59f74cda32f88ea77e01
Luigi de Brito Piantavinha <szochoralewizc@hotmail.com>
2025-07-22 18:58:33 +0100

    cpp07 - ex01 - writing main

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,8 +6,17 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:35:57 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/22 18:36:00 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:56:36 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <iostream>
+#include <string>
+
+template<typename T, typename F>
+void	iter(T *array, size_t size, F f) {
+	for (size_t i = 0; i < size; i++)
+		f(array[i]);
+}
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,8 +6,12 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:36:01 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/22 18:36:02 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:58:14 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/iter.hpp"
 
+int main(void) {
+
+}
