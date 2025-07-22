2e609d0d9eb736a60a93686c6ec03c9267a60e4a
Luigi de Brito Piantavinha <szochoralewizc@hotmail.com>
2025-07-22 18:35:13 +0100

    cpp07 - ex00 - main.cpp OK

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,8 +6,30 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:33 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/22 18:30:34 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:35:06 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "whatever.hpp"
 
+int main (void) {
+	
+	int a = 2;
+	int b = 3;
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a,b) << std::endl;
+	std::cout << "max(a,b) = " << ::max(a,b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+	return 0;
+}
