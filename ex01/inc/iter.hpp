797169feb1b3f0ef287c194c47356a8b734a294f
Luigi de Brito Piantavinha <szochoralewizc@hotmail.com>
2025-07-22 19:15:43 +0100

    cpp07 - ex01 - OK

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:35:57 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/22 18:56:36 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/22 19:08:49 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,15 @@ template<typename T, typename F>
 void	iter(T *array, size_t size, F f) {
 	for (size_t i = 0; i < size; i++)
 		f(array[i]);
+}
+
+template<typename T>
+void	print(const T &a) {
+	std::cout << a << std::endl;
+}
+
+template<typename T>
+void	addAndPrint(T &a) {
+	a += 1;
+	std::cout << a << std::endl;
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:36:01 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/22 18:58:14 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/22 19:15:12 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,24 @@
 
 int main(void) {
 
+	int intArray[] = {0, 1, 2, 3, 4, 5, 6};
+	char charArray[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+	std::cout << "Test 00. Array of Ints" << std::endl;
+	size_t sizeArray = (sizeof(intArray) / sizeof(int));
+	iter(intArray, sizeArray, print<int>);
+	std::cout << std::endl;
+
+	std::cout << "Test 01. Adding Elements - Int" << std::endl;
+	iter(intArray, sizeArray, addAndPrint<int>);
+	std::cout << std::endl;
+	
+	std::cout << "Test 02. Array of Chars" << std::endl;
+	sizeArray = (sizeof(charArray) / sizeof(char));
+	iter(charArray, sizeArray, print<char>);
+	std::cout << std::endl;
+	
+	std::cout << "Test 03. Adding Elements - Char" << std::endl;
+	iter(charArray, sizeArray, addAndPrint<char>);
+	std::cout << std::endl;
 }
