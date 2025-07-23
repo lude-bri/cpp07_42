f00cd9446791b3cde9786d4ed42e2ad8aaf5620a
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-23 19:32:30 +0100

    cpp07 - ex02 - main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -8,8 +8,26 @@
 /*   Created: 2025/07/23 13:01:44 by lude-bri          #+#    #+#             */
 /*   Updated: 2025/07/23 13:01:46 by lude-bri         ###   ########.fr       */
 /*                                                                            */
+
+            bool elementsCorrect = true;
+            for (unsigned int i = 0; i < intArray.size(); i++) {
+                if (intArray[i] != i * 10) {
+                    elementsCorrect = false;
+                    break;
 /* ************************************************************************** */
 
 #include "../inc/Array.hpp"
+#include <iostream>
 
+void	printTestHeader(std::string &test) {
+	std::cout << YELLOW << "== " << test << " ==" << RESET << std::endl;
+}
 
+int main() {
+
+	printTestHeader("Test 00. Array of ints");
+	{
+		Array<int> emptyArray;
+
+	}
+}
