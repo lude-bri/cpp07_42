0b7665c6f2f37c20058dc2166b047ead332bafca
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-23 19:07:38 +0100

    cpp07 - ex02 - setup

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -15,11 +15,48 @@
 #include <iostream>
 #include <string>
 
+#ifdef DEBUG
+# define DEBUG_MSG(x) std::cout << "[DEBUG] : " << x << std::endl
+#else
+# define DEBUG_MSG(x)
+#endif
+
+
 template<typename T>
 class Array {
 
+protected:
+
+	T				*_array;
+	unsigned int	_len;
+
 public:
-	Array() {};
+	//Construction with no parameter
+	Array() {
+		DEBUG_MSG("A Constructor for Array was called"); 
+	}
+
+	//Constructor with an usigned int n as parameter
+	Array(unsigned int n) {
+		DEBUG_MSG("A Parametric Constructor for Array was called");
+		_array = new T[n]();
+	}
+
+	//Copy Constructor
+	Array(const Array &copy){
+		DEBUG_MSG("A Copy Constructor for Array was called");
+	}
+
+	//Assignment Operator
+	Array &operator=(const Array &copy){
+		DEBUG_MSG("An Assignment Operator for Array was called");
+
+	}
+
+	//Destructor
+	~Array() {
+		DEBUG_MSG("A Destructor for Array was called");
+	}
 
 
 };
